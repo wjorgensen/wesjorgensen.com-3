c10cf64221f52165c2a82efb5d963c129c126551
BuildTools <unconfigured@null.spigotmc.org>
2023-10-31 21:57:57 -0400

    trying to get this stupid button to work

--- javascript/home.js
+++ javascript/home.js
@@ -8,43 +8,50 @@ document.querySelectorAll('.pushable').forEach(button => {
     });
 });
 
-function openContent(modalId) {
+async function openContent(modalId) {
     contentContainer.style.zIndex = 4;
+    contentContainer.style.display = 'block';
     contentContainer.style.backgroundColor = getComputedStyle(document.querySelector(`[data-modal="${modalId}"]`)).backgroundColor;
     contentContainer.style.boxShadow = '8px 8px ' + darkenColor(contentContainer.style.backgroundColor, 20);
-    animateContentContainer();
+    document.querySelectorAll('.close-button').forEach(button => {
+        button.style.display = 'none';
+    });
+    await animateContentContainer();
+    displayX();
+}
+
+function displayX() {
+    document.querySelectorAll('.close-button').forEach(button => {
+        button.style.display = 'block';
+    });
 }
 
 document.querySelectorAll('.close-button').forEach(button => {
     button.addEventListener('click', function() {
-        // Traverse up the DOM to find the parent modal of this close button
-        let parent = this.parentElement;
-        while (!parent.classList.contains('modal-content')) {
-            parent = parent.parentElement;
-        }
-        // Hide the parent modal
-        parent.style.display = 'none';
+        contentContainer.style.display = 'none';
+        contentContainer.style.height = 0+"vh";
+        contentContainer.style.width = 0+"vw";
     });
 });
 
 function animateContentContainer() {
-    let pos = 0;
-    const id = setInterval(frame, 0.5);
-    function frame() {
-        if (pos === 85) { 
-            clearInterval(id);
-            // Show the close button after the animation
-            document.querySelectorAll('.close-button').forEach(button => {
-                button.style.display = 'block';
-            });
-        } else {
-            pos++;
-            contentContainer.style.height = pos + 'vh';
-            contentContainer.style.width = pos + 'vw';
-            contentContainer.style.left = 50 - (pos * 0.5) + 'vw';
-            contentContainer.style.top = 50 - (pos * 0.5) + 'vh';
+    return new Promise((resolve, reject) => {
+        let pos = 0;
+        const id = setInterval(frame, 0.5);
+        function frame() {
+            if (pos === 85) {
+                clearInterval(id);
+                console.log("done");
+                resolve();
+            } else {
+                pos++;
+                contentContainer.style.height = pos + 'vh';
+                contentContainer.style.width = pos + 'vw';
+                contentContainer.style.left = 50 - (pos * 0.5) + 'vw';
+                contentContainer.style.top = 50 - (pos * 0.5) + 'vh';
+            }
         }
-    }
+    });
 }
 
 function darkenColor(color, percent) {
