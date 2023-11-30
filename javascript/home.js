ffd25b35c1f64a601decf7bb510f4d9c89601baf
BuildTools <unconfigured@null.spigotmc.org>
2023-11-30 13:17:06 -0500

    Add styles and content to modal windows

--- javascript/home.js
+++ javascript/home.js
@@ -1,10 +1,11 @@
-const contentContainer = document.getElementById('content-container');
-var buttons = document.querySelectorAll('.pushable');
+
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelectorAll('.pushable').forEach(button => {
         button.addEventListener('click', function() {
             let modalId = this.getAttribute('data-modal');
             let modal = document.getElementById(modalId);
+            let content = document.getElementById(modalId + '-content');
+            document.body.setAttribute('data-active-modal', modalId);
 
             anime({
                 targets: modal,
@@ -21,6 +22,9 @@ document.addEventListener('DOMContentLoaded', function() {
                 update: function(anim) {
                     let scaleValue = anim.animations[2].currentValue;
                     modal.style.transform = `translate(-50%, -50%) scale(${scaleValue})`;
+                    if(scaleValue > 0.5) {
+                        content.style.display = 'grid';
+                    }
                 },
                 complete: function() {
                     modal.classList.add('active');
@@ -32,6 +36,8 @@ document.addEventListener('DOMContentLoaded', function() {
     document.querySelectorAll('.modal-content .close-button').forEach(button => {
         button.addEventListener('click', function() {
             let modal = this.closest('.modal-content');
+            let activeModalId = document.body.getAttribute('data-active-modal');
+            let content = document.getElementById(activeModalId + '-content');
             anime({
                 targets: modal,
                 width: ['85%', '0%'],  
@@ -40,6 +46,13 @@ document.addEventListener('DOMContentLoaded', function() {
                 opacity: [1, 0],
                 easing: 'easeInExpo',
                 duration: 800,
+                update: function(anim) {
+                    let scaleValue = anim.animations[2].currentValue;
+                    modal.style.transform = `translate(-50%, -50%) scale(${scaleValue})`;
+                    if(scaleValue < 0.5) {
+                        content.style.display = 'none';
+                    }
+                },
                 complete: function() {
                     modal.style.display = 'none';
                     modal.classList.remove('active');
